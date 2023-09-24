48fd11b6b6d4ab2befe00c937ab070d5c901e545
mehedidev <mdmehedi23234@gmail.com>
2023-09-24 22:39:30 +0600

    fhyr

--- src/Components/storage/AppContext.jsx
+++ src/Components/storage/AppContext.jsx
@@ -1,10 +1,11 @@
 import { createClient, groq } from 'next-sanity';
-import React, {  useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
+export const UserContext = createContext()
 
-
-const index = () => {
+const AppContext = ({ children }) => {
     const [author, setAuthor] = useState({})
+    const [partners, setPartners] = useState([])
     const projectId = 'kwzw2vfn';
     const dataset = 'production'
     const apiVersion = "2023-05-03";
@@ -14,7 +15,7 @@ const index = () => {
         apiVersion, // https://www.sanity.io/docs/api-versioning
         useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
       });
-      useEffect( () => {
+      useEffect(() => {
         client
           .fetch(
             groq`*[_type == "author"]{
@@ -34,24 +35,40 @@ const index = () => {
       }`
           )
           .then((data) => {
+            console.log(data);
             setAuthor(data[0])
             // Access the full array of navbar data here
           });
       }, []);
+      useEffect(() => {
+        client
+          .fetch(
+            groq`*[_type == "partners"]{
+        _id,
+        name,
+        slug,
+        mainImage{
+            asset->{
+                _id,
+                url
+            }
+        },
+        bio,
+        "dropdownData": dropdownData[]{
+          title
+        }
+      }`
+          )
+          .then((data) => {
+            console.log(data);
+            setPartners(data)
+            // Access the full array of navbar data here
+          });
+      }, []);
+    const value = {}
     return (
-        <div className=''>
-            <div style={{backgroundImage: `url(${author?.image?.asset?.url})`}} className='bg-image md:h-[75vh] lg:h-[90vh] w-full text-center flex flex-col items-center justify-center py-12 md:py-0'>
-                <div className='text-white'>
-                <h2 className='uppercase text-4xl md:text-5xl lg:text-7xl font-bold'>{author?.name}</h2>
-                <p className=' text-xl md:text-3xl font-semibold my-6'>{author?.slug?.current}</p>
-                <div className='mt-10'>
-                    <button className='bg-primary px-6 py-4 rounded text-xl mr-4'>GET A QOUTE</button>
-                    <button className='hidden md:inline bg-white text-black px-6 py-4 rounded text-lg'>EXPLORE MORE</button>
-                </div>
-                </div>
-            </div>
-        </div>
+        <UserContext.Provider value={value}> {children} </UserContext.Provider>
     );
 };
 
-export default index;
+export default AppContext;
