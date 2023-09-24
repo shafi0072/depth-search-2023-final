f9dd95aa30e8c384c16386d8d3baf448fc88d352
Manik <tonysins43@gmail.com>
2023-09-24 22:17:21 +0600

    s

--- src/Components/app/Home/Banner/index.jsx
+++ src/Components/app/Home/Banner/index.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
+import PlayCircleOutlineOutlinedIcon from "@mui/icons-material/PlayCircleOutlineOutlined";
 const index = () => {
-
-    return (
-        <div>
-            <div className='bg-image md:h-[75vh] lg:h-[90vh] w-full text-center flex flex-col items-center justify-center py-12 md:py-0'>
-                <div className='text-white'>
-                <h2 className='uppercase text-4xl md:text-5xl lg:text-7xl font-bold'>Welcome to depth Search</h2>
-                <p className=' text-xl md:text-3xl font-semibold my-6'>Unveiling Depths with Innovative Software Solutions!</p>
-                <div className='mt-10'>
-                    <button className='bg-primary px-6 py-4 rounded text-xl mr-4'>GET A QOUTE</button>
-                    <button className='hidden md:inline bg-white text-black px-6 py-4 rounded text-lg'>EXPLORE MORE</button>
-                </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="bg-image md:h-[75vh] lg:h-[90vh] w-full text-center flex flex-col items-center justify-center py-12 md:py-0">
+        <div className="text-white">
+          <h2 className="uppercase text-4xl md:text-5xl lg:text-7xl font-bold">
+            Welcome to depth Search
+          </h2>
+          <p className=" text-xl md:text-3xl font-semibold my-6">
+            Unveiling Depths with Innovative Software Solutions!
+          </p>
+          <div className="mt-10">
+            <button className="bg-primary px-6 py-4 rounded text-xl mr-4">
+              GET A QOUTE
+            </button>
+            <button className="hidden md:inline bg-white text-black px-6 py-4 rounded text-lg">
+              EXPLORE MORE
+            </button>
+          </div>
         </div>
-    );
+        <div className="mt-8 cursor-pointer animate-pulse relative ">
+            <div className="signal mt-1"></div>
+          <PlayCircleOutlineOutlinedIcon className="absolute top-1 right-0"
+            sx={{ fontSize: "100px", color: "white" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default index;
---
+++ src/Components/app/Home/Projects/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+
+const Projects = () => {
+  return (
+    <div className="py-16">
+      <div className="text-center">
+        <h4 className="uppercase text-2xl font-semibold ">
+          EXPLORE OUR PORTFOLIO
+        </h4>
+        <h2 className="text-4xl md:text-5xl my-3 font-semibold">
+          Featured Web Development Projects
+        </h2>
+        <p className="text-2xl font-semibold">
+          Custom B2B & B2C Digital Experiences Engineered To Perform
+        </p>
+      </div>
+      <div className="flex gap-12 items-center justify-center my-16">
+        <div className="flex flex-col items-center">
+          <StarBorderOutlinedIcon />
+          <h2>FEATURED</h2>
+        </div>
+        <div className="flex flex-col items-center">
+          <StarBorderOutlinedIcon />
+          <h2>FEATURED</h2>
+        </div>
+        <div className="flex flex-col items-center">
+          <StarBorderOutlinedIcon />
+          <h2>FEATURED</h2>
+        </div>
+        <div className="flex flex-col items-center">
+          <StarBorderOutlinedIcon />
+          <h2>FEATURED</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
