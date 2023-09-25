ea36f901b2d8b79d6d887cb91b74f4df32281c28
Manik <tonysins43@gmail.com>
2023-09-25 20:34:19 +0600

    s

--- src/Components/app/Home/CaseStudies/RightSide.jsx
+++ src/Components/app/Home/CaseStudies/RightSide.jsx
@@ -7,13 +7,13 @@ const RightSide = ({caseStudy}) => {
                 <h2 className='text-4xl font-semibold '>{caseStudy?.name}</h2>
                 <p className='mt-2 w-full lg:w-[350px] lg:text-2xl'>{caseStudy?.description}</p>
             </div>
-            <div className='mt-32 flex gap-8 md:gap-40 lg:gap-56'>
+            <div className='mt-32 flex justify-between'>
                 <div>
-                    <h1 className='text-6xl md:text-[200px] text-blue-500'>{caseStudy?.increase_in_overall}</h1>
+                    <h1 className='text-6xl md:text-[120px] lg:text-[200px] text-blue-500'>{caseStudy?.increase_in_overall}</h1>
                     <p className='md:ml-12 md:text-3xl'>Increase in overall traffic</p>
                 </div>
                 <div>
-                    <h1 className='text-6xl md:text-[200px] text-blue-500 '>{caseStudy?.  faster_loading_speed}</h1>
+                    <h1 className='text-6xl md:text-[120px] lg:text-[200px] text-blue-500 '>{caseStudy?.  faster_loading_speed}</h1>
                     <p className='md:ml-3 md:text-3xl'>Faster Loading Speed</p>
                 </div>
             </div>
