ac38d7c4986021c01a0169fb386e809d1f962805
mehedidev <mdmehedi23234@gmail.com>
2023-09-28 00:25:30 +0600

    fg

--- src/Components/app/About/About.jsx
+++ src/Components/app/About/About.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from './Banner/Banner';
+import { createClient, groq } from 'next-sanity';
+import Team from './Team';
 
 const About = () => {
+    const [teams, setTeams] = useState([])
+    const projectId = 'kwzw2vfn';
+    const dataset = 'production'
+    const apiVersion = "2023-05-03";
+    const client = createClient({
+        projectId,
+        dataset,
+        apiVersion, // https://www.sanity.io/docs/api-versioning
+        useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+      });
+      useEffect( () => {
+        client
+          .fetch(
+            groq`*[_type == "team"]{
+        _id,
+        id,
+        name,
+        position,
+        image{
+            asset->{
+                _id,
+                url
+            }
+        },
+        facebook,
+        linkedin,
+        github,
+        twitter
+      }`
+          )
+          .then((data) => {
+            // console.log({data});
+            setTeams(data)
+            // Access the full array of navbar data here
+          });
+      }, []);
     return (
         <div>
-            <Banner/>
+            <Banner teams={teams?.length}/>
+            <Team teams={teams}/>
         </div>
     );
 };
---
+++ src/Components/app/About/Team.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Team = ({ teams }) => {
+    return (
+        <div className='mt-[150px]  mx-10 mb-[50px]'>
+            <div className='text-center'>
+                <h2 className='text-[38px] font-bold'>Meet The Leadership Team</h2>
+                <p className='text-[22px] font-medium mt-5'>Top industry with proven experience who are hands- <br />on in managing projects</p>
+            </div>
+            <div className='mt-[70px] grid grid-cols-4'>
+                {
+                    teams?.sort((a, b) => a.id - b.id)?.map((item, index) => <div key={index} className='w-full h-[300px] md:h-[500px] relative'>
+                        <img src={item?.image?.asset?.url} className='w-full h-full object-cover ' style={{ filter: 'grayscale(100%)' }} alt="" />
+                        <div className='absolute bottom-0 left-0 right-0 ps-3 pb-3' style={{background: 'rgba(0,0,0,0.5)'}}>
+                            <h3 className='text-[22px] font-bold'>
+                                {item?.name}
+                            </h3>
+                            <p className='text-[18px]'>{item?.position}</p>
+
+                        </div>
+                    </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Team;
