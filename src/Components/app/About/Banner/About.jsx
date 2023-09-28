a952679e97c466d3a2bc24dc7e035c1f371cf7da
mehedidev <mdmehedi23234@gmail.com>
2023-09-28 22:45:01 +0600

    fhyrfh

--- src/Components/app/About/Banner/About.jsx
+++ src/Components/app/About/Banner/About.jsx
@@ -4,8 +4,8 @@ const About = ({ teams }) => {
     return (
         <>
             <h1 className='uppercase text-[20px] font-bold'>DEDICATED INDUSTRY EXPERTS WITH PROVEN TRACK RECORDS</h1>
-            <h2 className='text-[45px] font-bold my-5'>The Team You Want On Your Project</h2>
-            <div className='flex mx-10 justify-between mt-10'>
+            <h2 className='text-[35px] md:text-[45px] font-bold md:my-5'>The Team You Want On Your Project</h2>
+            <div className='flex mx-10 flex-wrap  justify-center md:justify-between mt-5 md:mt-10'>
                 <div>
                     <h3 className='text-[70px] font-bold text-blue-500'>Expert</h3>
                     <h4 className='text-[20px] font-bold'>INDUSTRY LEADERS</h4>
