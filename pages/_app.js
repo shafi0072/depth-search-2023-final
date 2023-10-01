44721b2a51dce66d84abce22696a6058104f44c7
Manik <tonysins43@gmail.com>
2023-10-01 20:08:56 +0600

    s

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import Layout from "@/src/Components/layout/index";
 import AppContext from "@/src/Components/storage/AppContext";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Analytics />
     </AppContext>
   );
 }
