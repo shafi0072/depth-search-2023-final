d7254544d379b924661b0afd12a3782aa4f44baf
shafi0072 <safi29317@gmail.com>
2023-12-28 00:56:24 +0600

    s

--- src/Components/shared/Navbar/MobileNav/MobileNavMenu.jsx
+++ src/Components/shared/Navbar/MobileNav/MobileNavMenu.jsx
@@ -9,15 +9,15 @@ const MobileNavMenu = () => {
       <ul className="">
         <Link href='/' className="border-b-2 block border-gray-200 p-3 text-xl font-semibold">Home</Link>
         <li onClick={() => router.push("/services")} className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Services</li>
-        <li className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Portfolio</li>
+        <li onClick={() => router.push("/projects")} className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Projects</li>
         {/* <<<<<<< HEAD */}
-        <li className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Blog</li>
+        {/* <li className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Blog</li> */}
         {/* <Link href='/case-studies' className="border-b-2 block border-gray-200 p-3 text-xl font-semibold">Case Studies</Link> */}
-        <Link href='/Blogs' className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Blog</Link>
-        <Link href='/case-studies' className="border-b-2 block border-gray-200 p-3 text-xl font-semibold">Case Studies</Link>
+        {/* <Link href='/Blogs' className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Blog</Link> */}
+        {/* <Link href='/case-studies' className="border-b-2 block border-gray-200 p-3 text-xl font-semibold">Case Studies</Link> */}
         {/* >>>>>>> 5def482b69b73dcb644b86a87ad133ba06ecce57 */}
         <Link href='/about' className="border-b-2 block border-gray-200 p-3 text-xl font-semibold">About</Link>
-        <li onClick={() => router.push("contact")} className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Contact</li>
+        <li onClick={() => router.push("/contact-us")} className="border-b-2 border-gray-200 p-3 text-xl font-semibold">Contact</li>
       </ul>
     </div>
   );
