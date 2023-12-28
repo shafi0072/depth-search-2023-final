a02b4a61ef1e2cfe3d515af8cf1dd384a819aab7
shafi0072 <safi29317@gmail.com>
2023-12-28 22:13:23 +0600

    s

--- src/Components/app/Home/Contact/ContactForm.jsx
+++ src/Components/app/Home/Contact/ContactForm.jsx
@@ -5,8 +5,13 @@ import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 import ChatBubbleOutlinedIcon from '@mui/icons-material/ChatBubbleOutlined';
+import { useForm, ValidationError } from '@formspree/react';
 import Swal from 'sweetalert2';
 const ContactForm = () => {
+    const [state, handleSubmit] = useForm("xbjqygpq");
+    if (state.succeeded) {
+        return <p className='uppercase text-xl text-green-500'>Thanks We Will Catch you shortly!</p>;
+    }
     const submitForm = e => {
         e.preventDefault();
 
@@ -30,14 +35,14 @@ const ContactForm = () => {
         })
             .then(res => res.json())
             .then(data => {
-                if(data){
+                if (data) {
                     Swal.fire({
                         position: 'center',
                         icon: 'success',
                         title: 'Your message has been sent successfully!!',
                         showConfirmButton: false,
                         timer: 1500
-                      })
+                    })
                 }
             })
             .catch(err => console.log({ err }))
@@ -47,25 +52,50 @@ const ContactForm = () => {
         <div>
             <h3 className='text-4xl font-semibold'>Start a conversation with us</h3>
             <div className='mt-12 '>
-                <form className='space-y-10' onSubmit={submitForm}>
+                <form onSubmit={handleSubmit} className='space-y-10' >
                     <div className='w-full relative'>
                         <input className='border-b-2 ps-8 pb-3 bg-transparent w-full outline-none' name='name' type="text" placeholder='Name*' />
+                        <ValidationError
+                            prefix="Name"
+                            field="name"
+                            errors={state.errors}
+                        />
                         <Person3OutlinedIcon className='absolute left-0' />
                     </div>
                     <div className='w-full relative'>
                         <input className='border-b-2 ps-8 pb-3 bg-transparent w-full outline-none' name='companyName' type="text" placeholder='Company Name*' />
+                        <ValidationError
+                            prefix="Company Name"
+                            field="companyName"
+                            errors={state.errors}
+                        />
                         <HomeOutlinedIcon className='absolute left-0' />
                     </div>
                     <div className='w-full relative'>
                         <input className='border-b-2 ps-8 pb-3 bg-transparent w-full outline-none' name='email' type="text" placeholder='Email*' />
+                        <ValidationError
+                            prefix="Email"
+                            field="email"
+                            errors={state.errors}
+                        />
                         <EmailOutlinedIcon className='absolute left-0' />
                     </div>
                     <div className='w-full relative'>
                         <input className='border-b-2 ps-8 pb-3 bg-transparent w-full outline-none' type="text" name='number' placeholder='Number*' />
+                        <ValidationError
+                            prefix="Number"
+                            field="number"
+                            errors={state.errors}
+                        />
                         <LocalPhoneOutlinedIcon className='absolute left-0' />
                     </div>
                     <div className='w-full relative'>
                         <textarea className='bg-transparent border-b-2 pb-3 ps-8  outline-none h-24 w-full' name="message" id="" placeholder='Your Message'></textarea>
+                        <ValidationError
+                            prefix="Message"
+                            field="message"
+                            errors={state.errors}
+                        />
                         <ChatBubbleOutlinedIcon className='absolute left-0' />
                     </div>
                     <div className='flex justify-end mt-4'>
